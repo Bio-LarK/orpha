79ce3b3eba9778324b8138f50d8439edac8706f1
Craig <cmcnamara87@gmail.com>
2014-08-25 11:32:59 +1000

    fix(build): disable bump push

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,11 +76,11 @@ module.exports = function (grunt) {
                 updateConfigs: [],
                 commit: true,
                 commitMessage: 'Release v%VERSION%',
-                commitFiles: ['package.json', 'CHANGELOG.md'],
+                commitFiles: ['-a'],
                 createTag: true,
                 tagName: 'v%VERSION%',
                 tagMessage: 'Version %VERSION%',
-                push: true,
+                push: false,
                 pushTo: 'origin',
                 gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
             }
