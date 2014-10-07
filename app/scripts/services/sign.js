fc716535b211371497a407bf57131bf41355387b
Craig <cmcnamara87@gmail.com>
2014-10-07 15:54:45 +1000

    fix(sign-service): fix base url

--- app/scripts/services/sign.js
+++ app/scripts/services/sign.js
@@ -8,14 +8,13 @@
  * Factory in the orphaApp.
  */
 angular.module('orphaApp')
-    .factory('Sign', function ($resource, Gene, RelationshipService) {
-        var Sign = $resource('http://130.56.248.140/orphanet/api/entity_node/:nid', {
-            'parameters[type]': 'clinical_sign',
+    .factory('Sign', function ($resource, ENV, Gene, RelationshipService) {
+        var Sign = $resource(ENV.apiEndpoint + '/entity_node/:nid', {
+            'parameters[type]': 'disorder',
             nid: '@nid'
         });
 
         angular.extend(Sign.prototype, {});
-
 
         return Sign;
     });
