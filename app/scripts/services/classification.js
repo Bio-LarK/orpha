06327968d57ffb7ed3e59a0027c1683f3843e114
Craig <cmcnamara87@gmail.com>
2014-10-14 16:47:24 +1000

    style(disorder): add rainbow palette

--- app/scripts/services/classification.js
+++ app/scripts/services/classification.js
@@ -8,8 +8,22 @@
  * Factory in the orphaApp.
  */
 angular.module('orphaApp')
-  .factory('Classification', function($resource, $q, ENV) {
+    .factory('Classification', function($resource, $q, ENV, $log) {
         var colors = {};
+
+        var colorOptions = [];
+
+        for (var i = 0; i < 31; i++) {
+            var hue = 12 * i;
+            var sat = 90;
+            var lightness = 70;
+            colorOptions.push('hsla(' + hue + ', ' + sat + '%, ' + lightness + '%, 1)');
+
+            // var rgb = hslToRgb(hue, sat, lightness);
+            // $log.debug('rgb', rgb);
+            // $log.debug('lum is', 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[1] / 255);
+        }
+        var pointer = 0;
 
         var Classification = $resource(ENV.apiEndpoint + '/entity_node/:nid', {
             'parameters[type]': 'disorder_classification',
@@ -21,7 +35,7 @@ angular.module('orphaApp')
 
         function getAll() {
 
-        	return $q.all([Classification.query({
+            return $q.all([Classification.query({
                 page: 0
             }).$promise, Classification.query({
                 page: 1
@@ -36,9 +50,44 @@ angular.module('orphaApp')
             /* jshint validthis: true */
             var classification = this;
             var id = classification.id || classification.nid;
-            if(!colors[id]) {
-                colors[id] = '#'+Math.floor(Math.random()*16777215).toString(16); 
+            if (!colors[id]) {
+                // 11.6
+                if (pointer < colorOptions.length) {
+                    colors[id] = {
+                        order: pointer,
+                        color: colorOptions[pointer++],
+                    };
+                } else {
+                    colors[id] = '#' + Math.floor(Math.random() * 16777215).toString(16);
+                }
             }
-            return colors[id];
+            classification.hueIndex = colors[id].order;
+            return colors[id].color;
         }
+
+        // function hslToRgb(h, s, l) {
+        //     var r, g, b;
+
+        //     if (s == 0) {
+        //         r = g = b = l; // achromatic
+        //     } else {
+        //         function hue2rgb(p, q, t) {
+        //             if (t < 0) t += 1;
+        //             if (t > 1) t -= 1;
+        //             if (t < 1 / 6) return p + (q - p) * 6 * t;
+        //             if (t < 1 / 2) return q;
+        //             if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        //             return p;
+        //         }
+
+        //         var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        //         var p = 2 * l - q;
+        //         r = hue2rgb(p, q, h + 1 / 3);
+        //         g = hue2rgb(p, q, h);
+        //         b = hue2rgb(p, q, h - 1 / 3);
+        //     }
+
+        //     return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+        // }
+
     });
