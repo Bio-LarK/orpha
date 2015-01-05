9b5696ba45f2e9d05d9992396da2e6fafa2ea82a
Craig <cmcnamara87@gmail.com>
2015-01-05 13:43:03 +1000

    refactor(tests): change user test

--- test/spec/services/user.js
+++ test/spec/services/user.js
@@ -1,18 +1,18 @@
 'use strict';
 
-describe('Service: User', function () {
+describe('Service: User', function() {
 
-  // load the service's module
-  beforeEach(module('orphaApp'));
+    // load the service's module
+    beforeEach(module('orphaApp'));
 
-  // instantiate service
-  var user;
-  beforeEach(inject(function (_User_) {
-    user = _User_;
-  }));
+    // instantiate service
+    var user;
+    beforeEach(inject(function(_User_) {
+        user = _User_;
+    }));
 
-  it('should do something', function () {
-    expect(!!user).toBe(true);
-  });
+    it('should exist', function() {
+        expect(!!user).toBe(true);
+    });
 
 });
