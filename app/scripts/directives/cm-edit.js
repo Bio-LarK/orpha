d62eab0c693b5ab69ed2bcd21e6bd4b1ffb4180a
Craig <cmcnamara87@gmail.com>
2015-02-20 11:15:27 +1000

    refactor(disorder): change gene card to using cm-edit

--- app/scripts/directives/cm-edit.js
+++ app/scripts/directives/cm-edit.js
@@ -9,22 +9,71 @@
 angular.module('orphaApp')
     .directive('cmEdit', function () {
         return {
-            template: '<div ng-click="editing && click()"><div ng-transclude></div></div>',
-            restrict: 'E',
-            transclude: true,
+            //template: '<div ng-transclude></div>',
+            restrict: 'A',
+            //transclude: true,
             scope: {
                 editing: '=',
-                click: '&'
+                click: '&',
+                border: '@?'
             },
-            replace: true,
+            //replace: true,
             link: function postLink($scope, element, attrs) {
-                $scope.$watch('editing', function(isEditing) {
+
+                $scope.$watch('editing', editingStateChanged);
+
+                function editingStateChanged(isEditing) {
                     if(isEditing) {
-                        element.addClass('editable');
-                    } else {
-                        element.removeClass('editable');
+                        if($scope.border == 'bottom') {
+                            element.addClass('editable');
+                        } else {
+                            element.addClass('editable-card');
+                        }
+
+                        element.click(editClickHandler);
+                        disableInternalLinks(element);
+                        return;
                     }
-                });
+                    if($scope.border == 'bottom') {
+                        element.removeClass('editable');
+                    } else {
+                        element.removeClass('editable-card');
+                    }
+                    element.unbind('click', editClickHandler);
+                    enableInternalLinks(element);
+                }
+
+                /**
+                 * Call the passed in click method, cause we are editing :)
+                 */
+                function editClickHandler() {
+                    $scope.click();
+                }
+
+                /**
+                 * Disable internal links while we are editing
+                 * @param element
+                 */
+                function disableInternalLinks(element) {
+                    element.find('a').click(disableClickHandler);
+                }
+
+                /**
+                 * Handle anchors being clicked, while disabled
+                 * @param event
+                 */
+                function disableClickHandler(event) {
+                    event.preventDefault();
+                }
+
+                /**
+                 * Removes any disabling click handlers
+                 * @param element
+                 */
+                function enableInternalLinks(element) {
+                    element.find('a').unbind('click', disableClickHandler);
+                }
+
             }
         };
     });
